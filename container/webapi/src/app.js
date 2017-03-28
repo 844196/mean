7f55f0467a126ff3aff645650650ef38a4053035
Masaya Takeda <844196@gmail.com>
2017-03-29 01:54:26 +0900

    サーバ側ロガー差し込み

--- container/webapi/src/app.js
+++ container/webapi/src/app.js
@@ -2,12 +2,14 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var cors = require('cors')
+var morgan = require('morgan')
 var router = require('./Routes')
 var db = require('../config/db')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(morgan('tiny'))
 app.use(router)
 
 module.exports = app
