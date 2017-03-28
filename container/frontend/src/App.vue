0c34638e3e92354df37fc1ae0c65356e3d8b0b83
Masaya Takeda <844196@gmail.com>
2017-03-29 01:54:50 +0900

    最低限のCRUD操作を行えるようにクライアントを実装

--- container/frontend/src/App.vue
+++ container/frontend/src/App.vue
@@ -1,46 +1,97 @@
 <template lang="pug">
 #app
-  md-whiteframe
-    md-toolbar
-      h1.md-title Todo
+  md-whiteframe(md-tag='md-toolbar', md-elevation='2')
+    h2.md-title Todo
   main
     md-table
       md-table-header
         md-table-row
+          md-table-head.md-table-selection
           md-table-head Name
-          md-table-head isDone
+          md-table-head Actions
       md-table-body
         md-table-row(v-for='(todo, idx) in this.list', :key='todo.id')
+          md-table-cell.md-table-selection
+            md-checkbox(v-model='todo.isDone', @change.self='toggleTodoStatus(todo.id, !todo.isDone)')
           md-table-cell {{ todo.name }}
-          md-table-cell {{ todo.isDone }}
-    md-snackbar(md-position='bottom right', md-duration='4000', ref='notice')
+          md-table-cell
+            .todo-actions
+              md-button.md-icon-button(@click.native='openDialog("Edit todo", todo.id, todo.name)')
+                md-icon edit
+              md-button.md-icon-button(@click.native='deleteTodo(todo.id)')
+                md-icon delete
+    md-button.md-fab.md-fab-bottom-right(@click.native='openDialog("Add todo", "", "")')
+      md-icon add
+    md-snackbar(md-position='top right', md-duration='1000', ref='notice')
       span {{ notifyMessage }}
+    md-dialog-prompt(v-model='dialog.value', :md-title='dialog.title', @close='closeDialog', ref='upsertTodo')
 </template>
 
 <script>
 export default {
   name: 'app',
   data() {
-    return {notifyMessage: ''}
+    return {
+      notifyMessage: '',
+      dialog: {
+        targetId: '',
+        title: '',
+        value: '',
+      },
+    }
   },
   computed: {
     list() {
-      return this.$store.state.list
+      return this.$store.getters.sortedList
     },
   },
   mounted() {
-    this.getList()
+    this.updateList()
   },
   methods: {
-    getList() {
-      this.$store.dispatch('getList')
-        .catch((err) => {
-          this.notify(err.message)
-        })
+    action(type, payload, callback) {
+      this.$store.dispatch(type, payload)
+        .then(callback)
+        .catch(({ message }) => this.notify(message))
+    },
+    updateList() {
+      this.action('updateList', {})
+    },
+    createTodo(name) {
+      this.action('createTodo', {name, isDone: false}, (result) => {
+        this.notify('Created todo!')
+      })
+    },
+    renameTodo(id, name) {
+      this.action('updateTodo', {id, patch: {name}}, (result) => {
+        this.notify('Renamed todo!')
+      })
+    },
+    toggleTodoStatus(id, isDone) {
+      this.action('updateTodo', {id, patch: {isDone}}, (result) => {
+        this.notify('Updated todo status!')
+      })
+    },
+    deleteTodo(id) {
+      this.action('deleteTodo', {id}, (result) => {
+        this.notify('Deleted todo!')
+      })
     },
     notify(message) {
       this.notifyMessage = message
       this.$refs.notice.open()
+    },
+    openDialog(title, targetId, value) {
+      this.dialog = {title, targetId, value}
+      this.$refs['upsertTodo'].open()
+    },
+    closeDialog(context) {
+      if (context === 'cancel') return
+      if (this.dialog.targetId) {
+        this.renameTodo(this.dialog.targetId, this.dialog.value)
+      } else {
+        this.createTodo(this.dialog.value)
+      }
     },
   },
 }
@@ -51,4 +102,8 @@ main
   margin: 0 auto
   padding: 0 5px
   max-width: 996px
+.todo-actions
+  justify-content: inherit
+  .md-icon
+    position: inherit
 </style>
--- container/frontend/src/store/index.js
+++ container/frontend/src/store/index.js
@@ -10,31 +10,77 @@ const state = {
   list: [],
 }
 
+const getters = {
+  sortedList(state) {
+    return state.list.sort((itemA, itemB) => {
+      if (itemA.isDone === itemB.isDone) return 0
+      return itemA.isDone ? 1 : -1
+    })
+  },
+}
+
 const mutations = {
   updateList(state, payload) {
     state.list = payload.list
   },
+  addTodo(state, { todo }) {
+    state.list.push(todo)
+  },
+  updateTodo(state, { updatedTodo }) {
+    let before = state.list.find((beforeItem) => {
+      return beforeItem.id === updatedTodo.id
+    })
+    Object.keys(before).forEach((key) => {
+      before[key] = updatedTodo[key]
+    })
+  },
+  deleteTodo(state, { deletedId }) {
+    const idx = state.list.findIndex((item) => {
+      return item.id === deletedId
+    })
+    state.list.splice(idx, 1)
+  },
 }
 
 const actions = {
-  getList(context) {
-    return new Promise((resolve, reject) => {
-      api
-        .get('/todos')
-        .then((result) => {
-          context.commit('updateList', {list: result.data})
-          resolve(result)
-        })
-        .catch((err) => {
-          reject(err)
-        })
-    })
-  }
+  updateList(context) {
+    return api
+      .get('/todos')
+      .then((result) => {
+        context.commit('updateList', {list: result.data})
+        Promise.resolve(result)
+      })
+  },
+  createTodo(context, newTodo) {
+    return api
+      .post(`/todos`, newTodo)
+      .then((result) => {
+        context.commit('addTodo', {todo: result.data})
+        Promise.resolve(result)
+      })
+  },
+  updateTodo(context, { id, patch }) {
+    return api
+      .patch(`/todos/${id}`, patch)
+      .then((result) => {
+        context.commit('updateTodo', {updatedTodo: result.data})
+        Promise.resolve(result)
+      })
+  },
+  deleteTodo(context, { id }) {
+    return api
+      .delete(`/todos/${id}`)
+      .then((result) => {
+        context.commit('deleteTodo', {deletedId: id})
+        Promise.resolve(result)
+      })
+  },
 }
 
 Vue.use(Vuex)
 export default new Vuex.Store({
   state,
+  getters,
   mutations,
   actions,
 })
